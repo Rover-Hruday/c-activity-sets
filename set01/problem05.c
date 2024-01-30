eb3b0a2f9fbded73431ef145d8672b1c02799e42
Rover-Hruday <141307774+Rover-Hruday@users.noreply.github.com>
2024-01-30 10:03:18 +0530

    Update problem05.c

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,37 @@
-
+#include<stdio.h>
+int input()
+{
+    int num;
+    printf("enter a number:");
+    scanf("%d",&num);
+    return num;
+}
+int compare(int a,int b,int c)
+{
+    int largest=a;
+    {
+        if (b>largest);
+        {
+            largest=b;
+        }
+        if (c>largest);
+        {
+            largest=c;
+        }
+        return largest;
+    }
+}
+void output(int a , int b, int c, int largest)
+{
+    printf("the largest among %d, %d, %d is %d \n", a,b,c,largest);
+}
+int main()
+{
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    largest=compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+}
