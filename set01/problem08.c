ef01f643bff0e76592c2c598d68af19762f16c46
Rover-Hruday <141307774+Rover-Hruday@users.noreply.github.com>
2024-02-01 10:12:25 +0530

    Update problem08.c

---
+++ set01/problem08.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int input_array_size() 
+{
+  int n;
+  printf("Enter the number of elements: ");
+  scanf("%d", &n);
+  return n;
+}
+void input_array(int n, int a[n])
+{
+  printf("Enter the elements: ");
+  for (int i = 0; i < n; i++) 
+  {
+    scanf("%d", &a[i]);
+  }
+}
+int sum_n_array(int n, int a[n]) 
+{
+    int sum=0;
+  for (int i = 0; i < n; i++) 
+  {
+    sum += a[i];
+  }
+  return sum;
+}
+void output(int n, int a[n], int sum) 
+{
+  printf("The sum of array is: %d\n", sum);
+}
+int main()  
+{
+  int n = input_array_size();
+  int a[n];
+  input_array(n,a);
+  int sum = sum_n_array(n, a);
+  output(n, a, sum);
+  return 0;
+}
