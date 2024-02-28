73528e8a8196af93b64f417a4cb065653e3638c8
Rover-Hruday <141307774+Rover-Hruday@users.noreply.github.com>
2024-02-28 23:07:47 +0530

    Update problem02.c

---
+++ set02/problem02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int input_side()
+{
+  int side;
+  printf("Enter the side of the triangle\n");
+  scanf( "%d",&side);
+  return side;
+}
+
+int check_scalene(int a,int b,int c)
+{
+  if(a!=b && b!=c && c!=a)
+    
+}
